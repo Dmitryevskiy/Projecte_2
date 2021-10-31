400a6744d8dbce64ec5737681cf6dfb6f42122dd
Блажевич Дмитрий <dima1988_bl@mail.ru>
2021-10-31 20:55:36 +0300

    Р5

---
+++ Projectes/Proj_5/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("hello guy");
+int count = 0;
+int distanse = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+while  (distanse > 10)
+{
+    if (friend == 1)
+    {
+        time = distanse/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distanse/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    } 
+
+    distanse = distanse-(firstFriendSpeed+secondFriendSpeed)*time;
+    count = count+1;
+}
+
+Console.WriteLine ($"Собака пробежит {count} раз");
+   
+
+
+
